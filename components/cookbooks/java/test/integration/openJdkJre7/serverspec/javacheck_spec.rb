80cae770840a1d114c89c9727595bede2ad4a826
Cody Lee White <cody.white0@walmart.com>
2017-08-30 19:26:33 -0500

    added more testing to open jdk

--- components/cookbooks/java/test/integration/openJdkJre7/serverspec/javacheck_spec.rb
+++ components/cookbooks/java/test/integration/openJdkJre7/serverspec/javacheck_spec.rb
@@ -1,19 +1,14 @@
 require 'spec_helper'
 
-describe command('echo $JAVA_HOME | grep java') do
+describe command('java -version | grep OpenJDK') do
   its(:exit_status) { should eq 0 }
 end
-
+describe command('java -version | grep 1.6') do
+  its(:exit_status) { should eq 0 }
+end
 describe file($node['java']['install_dir']) do
   it { should be_directory }
   it { should be_mode 755 }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
-end
-
-describe file('/etc/profile.d/java.sh') do
-  it { should be_file }
-  it { should be_mode 644 }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
 end
